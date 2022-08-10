c48b74e093f559e17bb583e0aec91751915cca37
satinder <satindersingh472@gmail.com>
2022-08-09 18:37:22 -0600

    started working

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <life-cycle-example></life-cycle-example>
+  </div>
+</template>
+
+<script>
+import LifeCycleExample from "@/components/lifeCycleExample";
+
+export default {
+  name: "App",
+  components: {
+    LifeCycleExample,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/lifeCycleExample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="heading">This is the heading</h1>
+    <p class="below_heading">This is below heading</p>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate: function () {
+   let box = document.getElementsByClassName(`heading`); 
+    box[`innerHTML`][`style`][`color`] = `red`;
+  },
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
